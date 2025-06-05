c9baf853d97fd55f9cbe81a9d52ec0111be6834b
Rodrigo Bruno <rbruno.10@hotmail.com>
2025-06-04 21:34:09 -0300

    first commit

---
+++ src/test/java/com/senai/oficinaroluka/OficinarolukaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.oficinaroluka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficinarolukaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
